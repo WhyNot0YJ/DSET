076fb56bcb03ca4f575d3516b6f0f0f9512e69a4
WhyNot0YJ <3459646039@qq.com>
2025-11-16 21:37:10 +0800

    Add TrainingVisualizer import to MOE-RTDETR initialization for enhanced visualization capabilities

--- experiments/moe-rtdetr/src/misc/__init__.py
+++ experiments/moe-rtdetr/src/misc/__init__.py
@@ -3,5 +3,6 @@
 
 from .logger import *
 from .visualizer import *
+from .training_visualizer import TrainingVisualizer
 from .dist_utils import setup_seed, setup_print
 from .profiler_utils import stats
