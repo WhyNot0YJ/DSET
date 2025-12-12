07f23b2c210765d3ec03eb48376983719df16223
WhyNot0YJ <3459646039@qq.com>
2025-12-12 16:18:20 +0800

    Update batch size in `train_deformable_r18.py` from 16 to 12 to optimize training performance and resource allocation.

--- experiments/deformable-detr/train_deformable_r18.py
+++ experiments/deformable-detr/train_deformable_r18.py
@@ -196,7 +196,7 @@ def main():
 
     # Modify Batch Size and Num Workers
     # 设置 Batch Size 为 16，Num Workers 为 16
-    cfg.train_dataloader.batch_size = 16
+    cfg.train_dataloader.batch_size = 12
     cfg.train_dataloader.num_workers = 16
 
     # Val Dataloader
